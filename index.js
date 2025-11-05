e27420508211fe969bf4c676cc214bc05a8c38da
Azkalyasin <yasinadzkiaazkal@gmail.com>
2025-11-05 10:27:55 +0700

    menambahkan ednpoint put dan delete

--- index.js
+++ index.js
@@ -38,3 +38,32 @@ app.get("/hotel", async (req, res) => {
     res.send({ message: error.message });
   }
 });
+
+app.put("/hotel/:id", async (req, res) => {
+  const id = req.params.id;
+  const data = req.body;
+  try {
+    const hotel = await db.hotel.findByPk(id);
+    if (!hotel) {
+      res.status(404).send({ message: "hotel not found" });
+    }
+    await hotel.update(data);
+    res.send({ message: "hotel berhasil di update", hotel });
+  } catch (error) {
+    res.send({ message: error.message });
+  }
+});
+
+app.delete("/hotel/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const hotel = await db.hotel.findByPk(id);
+    if (!hotel) {
+      res.status(404).send({ message: "hotel not found" });
+    }
+    await hotel.destroy();
+    res.send({ message: "hotel berhasil dihapus" });
+  } catch (error) {
+    res.status(500).send(err);
+  }
+});
